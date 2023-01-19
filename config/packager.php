5ed3536d36b5087143b546a833f80314b3f9ba8b
Vladislav Osmianskij <vlaosm@gmail.com>
2023-01-19 11:37:12 +0200

    fix: Docker doesn't understand symlinks with absolute path that are created for locally developed packages

---
+++ config/packager.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    /*
+     * You can set defaults for the following placeholders.
+     */
+    'author_name' => 'Vlad Osmianski',
+    'author_email' => 'vlaosm@gmail.com',
+    'author_homepage' => 'https://osmianski.com/',
+    'license' => 'MIT',
+];
